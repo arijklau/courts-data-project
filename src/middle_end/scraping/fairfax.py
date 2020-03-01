391db68879decc227a4b74dd0df1ce76148cdd35
arijklau <arijklau@gmail.com>
2020-03-01 15:48:49 -0500

    basic front-end and query forming

---
+++ src/middle_end/scraping/fairfax.py
@@ -0,0 +1,7 @@
+
+# FAIRFAX COUNTY = https://eapps.courts.state.va.us/gdcourts/caseSearch.do?fromSidebar=true&searchLanding=searchLanding&searchType=hearingDate&searchDivision=T&searchFipsCode=059&curentFipsCode=059
+# FAIRFAX CITY = https://eapps.courts.state.va.us/gdcourts/caseSearch.do?fromSidebar=true&searchLanding=searchLanding&searchType=hearingDate&searchDivision=T&searchFipsCode=600&curentFipsCode=600
+
+# Need to fill out the form on the above links by entering a hearing date and clicking submit
+# Then parsing the table on the next page (and possibly clicking "next" to get the next page's data)
+
